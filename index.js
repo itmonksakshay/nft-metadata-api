853dd0debe65f41355346e8f50a3ec900d84217b
itmonksakshay <akshay.bhatt@hotmail.com>
2021-01-04 14:31:17 +0530

    Heroku Deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const db = require("./src/databse.json");
-const host="http://localhost:5000";
+const host="https://nft-metadata-api.herokuapp.com";
 
 const app = express();
 

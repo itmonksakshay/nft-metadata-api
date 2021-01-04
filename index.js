ff3326ba5c753994ae08b1ca34e58b12546357b6
itmonksakshay <akshay.bhatt@hotmail.com>
2021-01-04 13:34:33 +0530

    basic api server created

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const db = require("./src/databse.json");
+const host="http://localhost:5000";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res) {
+  res.send('Simple Api Server for ERC721 contract');
+});
+app.get('/api/token/:token_id', function(req, res) {
+  const tokenId = parseInt(req.params.token_id).toString();
+  const person = db[tokenId]
+  const data = {
+    'name': person.name,
+    'image': `${host}/images/${tokenId}.png`
+  };
+  res.send(data)
+});
+app.listen(process.env.PORT||3000,function(){
+	console.log("the server is running on port %s" ,this.address().port);
+});
